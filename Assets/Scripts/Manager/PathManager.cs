be9a494d5f2a22bce14aafa96a692d1350aca44f
BanMing <banming729@163.com>
2018-03-09 16:30:13 +0800

    pathmanager

---
+++ Assets/Scripts/Manager/PathManager.cs
@@ -0,0 +1,70 @@
+using System.IO;
+using UnityEngine;
+public class PathManager {
+    static string cacheDir = string.Empty;
+
+    static string streamingAssetPath2WWW = string.Empty;
+
+    static string luaFolderEditorPath = string.Empty;
+
+    static string luaZipPath = string.Empty;
+
+    static string assetsBunldePath = string.Empty;
+
+    public static string StreamingAssetPath2WWW {
+        get {
+            if (streamingAssetPath2WWW == string.Empty) {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+                streamingAssetPath2WWW = "file:///" + Application.streamingAssetsPath + "/";
+#else
+                streamingAssetPath2WWW = Application.streamingAssetsPath + "/";
+#endif
+            }
+            return streamingAssetPath2WWW;
+        }
+    }
+
+    public static string CacheDir {
+        get {
+            if (cacheDir == string.Empty) {
+#if UNITY_EDITOR || UNITY_STANDALONE
+                cacheDir = MyFileUtil.GetParentDir (Application.dataPath) + "/";
+#else
+                cacheDir = Application.persistentDataPath + "/";
+#endif
+                cacheDir += "Cache/";
+                MyFileUtil.CreateDir (cacheDir);
+            }
+            return cacheDir;
+        }
+    }
+
+    public static string LuaFolderEditorPath {
+        get {
+            if (luaFolderEditorPath == string.Empty) {
+                luaFolderEditorPath = Application.streamingAssetsPath + "/Lua";
+            }
+            return luaFolderEditorPath;
+        }
+    }
+
+    public static string LuaZipPath {
+        get {
+            if (luaZipPath == string.Empty) {
+                luaZipPath = AssetsBunldePath + "Code.zip";
+                File.Create (luaZipPath);
+            }
+            return luaZipPath;
+        }
+    }
+
+    public static string AssetsBunldePath {
+        get {
+            if (assetsBunldePath == string.Empty) {
+                assetsBunldePath = Application.streamingAssetsPath + "/AssetsBundles/" + LuaConst.osDir + "/";
+                MyFileUtil.CreateDir (assetsBunldePath);
+            }
+            return assetsBunldePath;
+        }
+    }
+}
