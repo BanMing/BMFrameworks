71f12ef0e00bbdacbb5720437691953043ab073e
BanMing <banming729@163.com>
2018-06-07 14:40:13 +0800

    sprite packer tool

--- Assets/Editor/SpritePackerEditorTool.cs
+++ Assets/Editor/SpritePackerEditorTool.cs
@@ -6,8 +6,41 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
-public class SpritePackerEditorTool
+public class SpritePackerEditorTool : EditorWindow
 {
+    static SpritePackerEditorTool window;
+    static string SpritesPath=ResourcesManager.DirForAssetBundlesBuildFrom;
+    private Vector2 scrollVector;
+    [MenuItem("Tools/Sprite Packer Editor")]
+    static void OpenSpritePackerEditor()
+    {
+        window = SpritePackerEditorTool.GetWindow<SpritePackerEditorTool>();
+        window.Show();
+        PlayerPrefs.GetString("SpritePackerEditorToolSpritesPath",SpritesPath.Remove(SpritesPath.IndexOf("Asset")));
+    }
+    void OnGUI()
+    {
+        var pathWidth = window.position.width * 2 / 3;
+        var normalWidth = window.position.width / 9;
+        GUILayout.BeginHorizontal("box");
+        {
+            GUILayout.Label("Path", GUILayout.Width(pathWidth));
+            GUILayout.Label("Tag", GUILayout.Width(normalWidth));
+            GUILayout.Label("Packer", GUILayout.Width(normalWidth));
+            GUILayout.Label("Del", GUILayout.Width(normalWidth));
+        }
+        GUILayout.EndHorizontal();
+        scrollVector = GUILayout.BeginScrollView(scrollVector);
+        {
+
+        }
+        GUILayout.EndScrollView();
+        SpritesPath=GUILayout.TextField(SpritesPath);
+        if (GUILayout.Button("Pack All Sprites"))
+        {
+
+        }
+    }
     // public 
     [MenuItem("Assets/Create/Create Current Select Sprite Packer")]
     private static void CreateSpritePacker()
@@ -22,6 +55,11 @@ public class SpritePackerEditorTool
             foreach (var itemPath in fileList)
             {
                 var newPath = itemPath.Substring(itemPath.IndexOf("Assets"));
+                var extension = Path.GetExtension(newPath);
+                if (extension.Equals(".asset"))
+                {
+                    continue;
+                }
                 var improter = AssetImporter.GetAtPath(newPath) as TextureImporter;
                 improter.textureType = TextureImporterType.Sprite;
                 improter.mipmapEnabled = false;
@@ -51,5 +89,10 @@ public class SpritePackerEditorTool
         packer.SpriteList.AddRange(sprites);
 
         return false;
+    }
+    class SpriteInfo
+    {
+        public string path;
+        public string tag;
     }
 }
